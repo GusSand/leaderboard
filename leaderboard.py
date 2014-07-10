b85f2860ebb8794a8223f0c2b50cab43e71290f8
Gustavo Sandoval <gustavo.sandoval@skype.net>
2014-07-10 08:47:40 -0400

    init

---
+++ leaderboard.py
@@ -0,0 +1,261 @@
+
+"""
+    Leaderboard Service
+    ~~~~~~~~~~~~~~~~~~~
+
+
+	A service that will return the statistics on which objects are the most liked
+	in our community. 
+	The service provides APIs for Like and unlike. 
+
+	Most importantly the service provides an API for that will  provide a means of querying 
+	for ranked list of the top K
+	images that have received the most likes in the following time periods: 
+	last 24 hours, => day
+	36 hours,  => 3rd day
+	1 week, => week
+	1 month, => month
+	1 year.=> year. 
+
+
+	Base URL: 
+		TODO: 
+
+
+	usage: 
+	API Expects:
+
+
+	returns:
+
+    200 - successful submission
+	400 - Bad request 
+	500 - Server error
+
+
+	:copyright: (c) 2014 by Gustavo Sandoval.
+
+"""
+import os
+import pymongo
+from flask import Flask, request, session, g, abort, make_response, Response, json, jsonify
+from urlparse import urlparse
+import datetime
+
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+
+
+
+###############################################################
+#
+#	Database stuff
+#
+###############################################################
+
+
+##
+## Opens a new db connection if there's none for the current app context
+## 
+def get_db():
+	app.logger.debug('Entered: get_db' )
+	if not hasattr(g, 'mongo_db'):
+		g.mongo_db = init_db()
+	return g.mongo_db
+
+##
+## Connects to the actual db
+##
+def init_db():
+	app.logger.debug('Entered: init_db' )
+	mongo_url = os.environ.get('MONGOHQ_URL')
+
+
+	if mongo_url:
+		# first try to see if we are running under heroku
+		conn = pymongo.Connection(mongo_url)
+		app.logger.debug('got heroku connection to mongo' )
+		db = conn[urlparse(mongo_url).path[1:]]
+
+	else:
+		# not on an app with MongoHQ, try localhost
+		conn = pymongo.Connection('localhost', 27017)
+		app.logger.debug('got localhost connection to mongo' )	
+		db = conn['leaderboard-db']
+
+
+	return db
+
+# TODO: teardown connections
+#@app.teardown_appcontext
+#def close_db(error):
+#	if hasattr(g, 'mongo_db'):
+		#g.mongo_db.close()
+
+
+###############################################################
+#
+#	Routes
+#
+###############################################################
+
+
+@app.errorhandler(400)
+def server_error():
+	return make_response(standard_response(None, 400, 'Sorry an Error Ocurred'), 400)
+
+
+###
+###	Create or liking an Image
+### 
+@app.route("/api/images/like", methods=['POST'])
+def images_like():
+	
+	error = None
+	app.logger.debug('Entered images_like. Payload: ' )
+	json = request.get_json()
+
+	app.logger.debug("json:")
+	app.logger.debug(json)
+
+	# First validate all the payload
+	if (json == None):
+		error = 'invalid payload'
+		return Response(status=400, response=error)
+
+	image = json.get('image')	
+	if (image == None):
+		error = 'invalid image id'
+		return Response(status=400, response=error)
+
+	user  = request.get_json().get('user')
+	if (user == None):
+		error == 'invalid user id'
+		return Response(status=400, response=error)
+
+
+	#everything ok, let's do work
+	
+	db = get_db()
+	likes = db['likes']
+	count = 1
+
+	# check to see if this exists in the db and how many likes it has
+	#key = likes.find_one( { "image_id" : image, likes.count: { '$exists': True }})
+	#TODO
+	key = likes.find_one( { "image_id" : image} )
+
+
+	if (key is None):
+
+		app.logger.debug('inserting new entry in db' )
+
+		like = {
+			'image_id' : image, 
+			'user_id' : user, 
+			'date_list' : [datetime.datetime.utcnow()],
+			'count' : count	
+		}	
+		like_id = likes.insert(like)
+		app.logger.debug('inserted in db' )
+
+	else:
+
+		app.logger.debug('updating in db' )
+		app.logger.debug(key)
+
+		try:			
+			count = key.count #['count']
+		except:			
+			count = count + 1
+
+		try:
+			datelist = key.date_list #['date_list']
+		except:
+			datelist = [datetime.datetime.utcnow()]
+
+
+		datelist.append(datetime.datetime.utcnow())
+
+		like_id = likes.update(
+				{ 	'image_id' : image, }, 
+				{
+					'image_id' : image, 
+					'user_id'  : user, 
+					'date_list' : datelist,
+					'count' : count					
+				}, 
+			)
+
+		app.logger.debug('updated in db' )
+
+
+	resp = jsonify(image_id=image, image_count=count)
+	return resp
+
+
+	
+
+###
+###	Route Unliking an Image
+### 
+@app.route("/api/images/leaderboard", methods=['GET'])
+def images_leaderboard():
+	app.logger.debug('Entered images_leaderboard. Payload: ' )
+	app.logger.debug(request.get_json())
+
+	try:
+		
+		db = get_db()
+		likes = db.likes.find_one()
+
+		#data = db.collection_names()
+		#app.logger.debug(data)
+
+		return str(likes) #jsonify(username=g.user.username)
+
+
+	except:
+		app.logger.debug('Server Error' )
+		abort(400)
+
+
+###
+###	Destroy or Unliking an Image
+### 
+@app.route("/api/images/unlike", methods=['POST'])
+def images_unlike():
+	app.logger.debug('Entered images_unlike. Payload: ' )
+	app.logger.debug(request.get_json())
+
+	try:
+
+
+		return 'ok'
+
+
+	except:
+		app.logger.debug('Server Error' )
+		abort(500)
+
+
+
+
+
+###
+### Root Route, doesn't do anything. just make sure we are loaded
+###
+@app.route('/')
+def hello():
+	return 'Please use the API'
+
+
+##
+## So that we can run it in the localhost
+if __name__ == '__main__':
+	app.run(debug=True)
+	#app.run()
---
+++ leaderboard_tests.py
@@ -0,0 +1,95 @@
+import os
+import sys
+import leaderboard
+import unittest
+import random
+from flask import json, jsonify
+
+
+class LeaderboardTestCase(unittest.TestCase):
+
+	JSON 		= 'application/json'
+	LIKE_API 	 = 'api/images/like'
+	UNLIKE_API   = 'api/images/unlike'
+	LEADERBOARD_API = 'api/images/leaderboard'
+
+
+	def setUp(self):
+		self.app = leaderboard.app.test_client()
+		assert self.app != None
+		#self.app.config['TESTING'] = True
+		leaderboard.init_db()
+
+
+
+
+	##
+	## simple test for unlike
+	#def test_unlike(self):
+
+	##
+	## test repeated likes
+	def test_multiple_like(self):
+
+		image_id = random.randint(1, sys.maxint)
+		mydata = json.dumps({'image':image_id, 'user':'123456'})
+		
+		for x in range (0, 3):
+			resp = self.app.post('/api/images/like', 
+				data = mydata, 
+				content_type = self.JSON)
+
+
+		print str(resp.data)
+		jdata = json.loads(resp.data)
+		self.assertEqual(resp.status_code, 200)
+		self.assertEqual(jdata['image_count'], 3)
+
+
+
+	##
+	## simple test for like 
+	def test_like(self):
+
+		mydata = json.dumps({'image':'123444', 'user':'2345666'})
+		resp = self.app.post('/api/images/like', 
+			data = mydata, 
+			content_type = self.JSON)
+
+		print str(resp.data)
+		self.assertEqual(resp.status_code, 200)
+		#assert (rv.status_code == 200)
+
+
+	#
+	# Test for passing invalid image id on the payload
+	def test_invalid_image(self):
+		mydata = json.dumps({'user':'2345'})
+		resp = self.app.post('api/images/like', 
+			data = mydata, 
+			content_type = self.JSON)
+
+		# Response should return a 400
+		print ('Error: ' + str(resp.data))
+		assert 'invalid image' in resp.data
+		self.assertEqual(resp.status_code, 400)
+
+
+	##
+	## Test when we don't pass any params. 
+	## We should pass image & user
+	def test_invalid_payload(self):
+		#assert self.app != None
+		#self.app = leaderboard.app.test_client()
+		
+		resp = self.app.post('/api/images/like')
+	
+		# Response should return a 400
+		print ('Error: ' + str(resp.data))
+		assert 'invalid payload' in resp.data
+		self.assertEqual(resp.status_code, 400)
+
+
+
+if __name__ == '__main__':
+	unittest.main()
